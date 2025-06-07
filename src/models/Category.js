84ea43ea71c097321f28a41a98a40cfaa786126b
hungcudai <hungcudai111@gmail.com>
2025-06-07 23:01:32 +0700

    category api

---
+++ src/models/Category.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const categorySchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    image: { type: Array, required: true },
+    views: { type: Number, default: 0 },
+    date: { type: Number, required: true },
+});
+
+export default mongoose.model("category", categorySchema);
