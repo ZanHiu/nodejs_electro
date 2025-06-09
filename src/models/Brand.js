8e0193e33d66080fa3a0e8c3570f3c6d388fabd0
nhatcatmoi <hoangnhat211002@gmail.com>
2025-06-09 10:04:22 +0700

    brand api

---
+++ src/models/Brand.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const brandSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  image: { type: Array, required: true },
+  views: { type: Number, default: 0 },
+  date: { type: Number, required: true },
+});
+
+export default mongoose.model("brand", brandSchema);
