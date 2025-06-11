3d9e6f3b50bba8a61a2c0973b5c93c5a76a4569f
nhatcatmoi <hoangnhat211002@gmail.com>
2025-06-11 10:47:20 +0700

    update routes

--- src/app.js
+++ src/app.js
@@ -47,16 +47,76 @@ app.get("/", (req, res) => {
   res.json({
     message: "Welcome to the API!",
     available_routes: [
-      { method: "GET", path: `${baseUrl}/api/categories` },
-      { method: "GET", path: `${baseUrl}/api/categories/:id` },
-      { method: "POST", path: `${baseUrl}/api/categories` },
-      { method: "PUT", path: `${baseUrl}/api/categories/:id` },
-      { method: "DELETE", path: `${baseUrl}/api/categories/:id` },
+      //blog
+      { method: "GET", path: `${baseUrl}/api/blogs/list` },
+      { method: "GET", path: `${baseUrl}/api/blogs/home` },
+      { method: "POST", path: `${baseUrl}/api/blogs/add` },
+      
+      //brand
+      { method: "GET", path: `${baseUrl}/api/brands/list` },
+      { method: "GET", path: `${baseUrl}/api/brands/top` },
+      { method: "GET", path: `${baseUrl}/api/brands/seller-list` },
+      { method: "POST", path: `${baseUrl}/api/brands/add` },
+      { method: "PUT", path: `${baseUrl}/api/brands/edit/:id` },
+      { method: "DELETE", path: `${baseUrl}/api/brands/delete/:id` },
+
+      //cart
+      { method: "GET", path: `${baseUrl}/api/cart/get` },
+      { method: "POST", path: `${baseUrl}/api/cart/updatet` },
+
+      //category
+      { method: "GET", path: `${baseUrl}/api/categories/list` },
+      { method: "GET", path: `${baseUrl}/api/categories/top` },
+      { method: "GET", path: `${baseUrl}/api/categories/seller-list` },
+      { method: "POST", path: `${baseUrl}/api/categories/add` },
+      { method: "PUT", path: `${baseUrl}/api/categories/edit/:id` },
+      { method: "DELETE", path: `${baseUrl}/api/categories/delete/:id` },
+      
+      //home
+      { method: "GET", path: `${baseUrl}/api/home` },
+      
+      //order
+      { method: "GET", path: `${baseUrl}/api/orders/list` },
+      { method: "GET", path: `${baseUrl}/api/orders/detail/:id` },
+      { method: "GET", path: `${baseUrl}/api/orders/seller-orders` },
+      { method: "POST", path: `${baseUrl}/api/orders/verify-payment` },
+      { method: "PUT", path: `${baseUrl}/api/orders/cancel/:id` },
+      { method: "PUT", path: `${baseUrl}/api/orders/update-status/:id` },
+      
+      //payment
+      { method: "GET", path: `${baseUrl}/api/payments/vnpay-return` },
+      { method: "POST", path: `${baseUrl}/api/payments/create-vnpay-payment` },
+      
+      //product
+      { method: "GET", path: `${baseUrl}/api/products/list` },
+      { method: "GET", path: `${baseUrl}/api/products/seller-list` },
+      { method: "GET", path: `${baseUrl}/api/products/category/:id` },
+      { method: "GET", path: `${baseUrl}/api/products/brand/:id` },
+      { method: "GET", path: `${baseUrl}/api/products/filter` },
+      { method: "GET", path: `${baseUrl}/api/products/search` },
+      { method: "POST", path: `${baseUrl}/api/products/add` },
+      { method: "PUT", path: `${baseUrl}/api/products/edit/:id` },
+      { method: "DELETE", path: `${baseUrl}/api/products/delete/:id` },
+      
+      //review
+      { method: "GET", path: `${baseUrl}/api/reviews/blog/:blogId` },
+      { method: "POST", path: `${baseUrl}/api/reviews/blog` },
+      
+      //user
+      { method: "GET", path: `${baseUrl}/api/users/data` },
+      { method: "GET", path: `${baseUrl}/api/users/get-addresses` },
+      { method: "GET", path: `${baseUrl}/api/users/cart` },
+      { method: "GET", path: `${baseUrl}/api/users/seller-list` },
+      { method: "POST", path: `${baseUrl}/api/users/add-address` },
+      { method: "POST", path: `${baseUrl}/api/users/cart/update` },
+      { method: "PUT", path: `${baseUrl}/api/users/update-role/:userId` },
+      { method: "PUT", path: `${baseUrl}/api/users/toggle-block/:userId` },
+
     ],
   });
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running http://localhost:${PORT}`);
 });
