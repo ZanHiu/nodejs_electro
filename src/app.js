91b0bb6a42309733f865cc40689c8d53b26554ac
nhatcatmoi <hoangnhat211002@gmail.com>
2025-07-30 12:59:20 +0700

    update title routes

--- src/app.js
+++ src/app.js
@@ -60,117 +60,180 @@ app.get("/", (req, res) => {
   res.json({
     message: "Welcome to the API!",
     available_routes: [
-      //blog
-      { method: "GET", path: `${baseUrl}/api/blogs/list` },
-      { method: "GET", path: `${baseUrl}/api/blogs/home` },
-      { method: "GET", path: `${baseUrl}/api/blogs/:id` },
-      { method: "POST", path: `${baseUrl}/api/blogs/add` },
-      { method: "PUT", path: `${baseUrl}/api/blogs/edit/:id` },
-      { method: "DELETE", path: `${baseUrl}/api/blogs/delete/:id` },
+      {
+        name: "blog",
+        routes: [
+          { method: "GET", path: `${baseUrl}/api/blogs/list` },
+          { method: "GET", path: `${baseUrl}/api/blogs/home` },
+          { method: "GET", path: `${baseUrl}/api/blogs/:id` },
+          { method: "POST", path: `${baseUrl}/api/blogs/add` },
+          { method: "PUT", path: `${baseUrl}/api/blogs/edit/:id` },
+          { method: "DELETE", path: `${baseUrl}/api/blogs/delete/:id` },
+        ]
+      },
       
-      //brand
-      { method: "GET", path: `${baseUrl}/api/brands/list` },
-      { method: "GET", path: `${baseUrl}/api/brands/top` },
-      { method: "GET", path: `${baseUrl}/api/brands/seller-list` },
-      { method: "POST", path: `${baseUrl}/api/brands/add` },
-      { method: "PUT", path: `${baseUrl}/api/brands/edit/:id` },
-      { method: "PUT", path: `${baseUrl}/api/brands/update/:id` },
-      { method: "DELETE", path: `${baseUrl}/api/brands/delete/:id` },
-
-      //cart
-      { method: "GET", path: `${baseUrl}/api/cart/get` },
-      { method: "POST", path: `${baseUrl}/api/cart/update` },
-
-      //category
-      { method: "GET", path: `${baseUrl}/api/categories/list` },
-      { method: "GET", path: `${baseUrl}/api/categories/top` },
-      { method: "GET", path: `${baseUrl}/api/categories/seller-list` },
-      { method: "POST", path: `${baseUrl}/api/categories/add` },
-      { method: "PUT", path: `${baseUrl}/api/categories/edit/:id` },
-      { method: "PUT", path: `${baseUrl}/api/categories/update/:id` },
-      { method: "DELETE", path: `${baseUrl}/api/categories/delete/:id` },
+      {
+        name: "brand",
+        routes: [
+          { method: "GET", path: `${baseUrl}/api/brands/list` },
+          { method: "GET", path: `${baseUrl}/api/brands/top` },
+          { method: "GET", path: `${baseUrl}/api/brands/seller-list` },
+          { method: "POST", path: `${baseUrl}/api/brands/add` },
+          { method: "PUT", path: `${baseUrl}/api/brands/edit/:id` },
+          { method: "PUT", path: `${baseUrl}/api/brands/update/:id` },
+          { method: "DELETE", path: `${baseUrl}/api/brands/delete/:id` },
+        ]
+      },
       
-      //comment
-      { method: "GET", path: `${baseUrl}/api/comments/:type/:targetId` },
-      { method: "POST", path: `${baseUrl}/api/comments/` },
-      { method: "PUT", path: `${baseUrl}/api/comments/:commentId` },
-      { method: "DELETE", path: `${baseUrl}/api/comments/:commentId` },
+      {
+        name: "cart",
+        route: [
+          { method: "GET", path: `${baseUrl}/api/cart/get` },
+          { method: "POST", path: `${baseUrl}/api/cart/update` },
+        ]
+      },
 
-      //coupon
-      { method: "GET", path: `${baseUrl}/api/coupons/public` },
-      { method: "GET", path: `${baseUrl}/api/coupons/list` },
-      { method: "GET", path: `${baseUrl}/api/coupons/:id` },
-      { method: "POST", path: `${baseUrl}/api/coupons/validate` },
-      { method: "POST", path: `${baseUrl}/api/coupons/create` },
-      { method: "PUT", path: `${baseUrl}/api/coupons/update/:id` },
-      { method: "DELETE", path: `${baseUrl}/api/coupons/delete/:id` },
+      {
+        name: "category",
+        route: [
+          { method: "GET", path: `${baseUrl}/api/categories/list` },
+          { method: "GET", path: `${baseUrl}/api/categories/top` },
+          { method: "GET", path: `${baseUrl}/api/categories/seller-list` },
+          { method: "POST", path: `${baseUrl}/api/categories/add` },
+          { method: "PUT", path: `${baseUrl}/api/categories/edit/:id` },
+          { method: "PUT", path: `${baseUrl}/api/categories/update/:id` },
+          { method: "DELETE", path: `${baseUrl}/api/categories/delete/:id` },
+        ]
+      },
 
-      //favorite
-      { method: "GET", path: `${baseUrl}/api/favorites/list` },
-      { method: "GET", path: `${baseUrl}/api/favorites/check/:productId` },
-      { method: "POST", path: `${baseUrl}/api/favorites/add` },
-      { method: "DELETE", path: `${baseUrl}/api/favorites/remove/:productId` },
+      {
+        name: "comment",
+        route: [
+          { method: "GET", path: `${baseUrl}/api/comments/:type/:targetId` },
+          { method: "POST", path: `${baseUrl}/api/comments/` },
+          { method: "PUT", path: `${baseUrl}/api/comments/:commentId` },
+          { method: "DELETE", path: `${baseUrl}/api/comments/:commentId` },
+        ]
+      },
 
-      //home
-      { method: "GET", path: `${baseUrl}/api/home/` },
+      {
+        name: "coupon",
+        route: [
+          { method: "GET", path: `${baseUrl}/api/coupons/public` },
+          { method: "GET", path: `${baseUrl}/api/coupons/list` },
+          { method: "GET", path: `${baseUrl}/api/coupons/:id` },
+          { method: "POST", path: `${baseUrl}/api/coupons/validate` },
+          { method: "POST", path: `${baseUrl}/api/coupons/create` },
+          { method: "PUT", path: `${baseUrl}/api/coupons/update/:id` },
+          { method: "DELETE", path: `${baseUrl}/api/coupons/delete/:id` },
+        ]
+      },
 
-      // //luckyDraw
-      // { method: "POST", path: `${baseUrl}/api/luckyDraws/spin` },
+      {
+        name: "favorite",
+        route: [
+          { method: "GET", path: `${baseUrl}/api/favorites/list` },
+          { method: "GET", path: `${baseUrl}/api/favorites/check/:productId` },
+          { method: "POST", path: `${baseUrl}/api/favorites/add` },
+          { method: "DELETE", path: `${baseUrl}/api/favorites/remove/:productId` }, 
+        ]
+      },
 
-      //openrouter (chatbox)
-      { method: "POST", path: `${baseUrl}/api/openrouters/chat` },
+      {
+        name: "home",
+        route: [
+          { method: "GET", path: `${baseUrl}/api/home/` },
+        ]
+      },
       
-      //order
-      { method: "GET", path: `${baseUrl}/api/orders/list` },
-      { method: "GET", path: `${baseUrl}/api/orders/pending` },
-      { method: "GET", path: `${baseUrl}/api/orders/processing` },
-      { method: "GET", path: `${baseUrl}/api/orders/delivered` },
-      { method: "GET", path: `${baseUrl}/api/orders/cancelled` },
-      { method: "GET", path: `${baseUrl}/api/orders/detail/:id` },
-      { method: "GET", path: `${baseUrl}/api/orders/seller-orders` },
-      { method: "GET", path: `${baseUrl}/api/orders/check-purchase/:productId` },
-      { method: "POST", path: `${baseUrl}/api/orders/create` },
-      { method: "POST", path: `${baseUrl}/api/orders/verify-payment` },
-      { method: "PUT", path: `${baseUrl}/api/orders/cancel/:id` },
-      { method: "PUT", path: `${baseUrl}/api/orders/update-status/:id` },
-      
-      //payment
-      { method: "GET", path: `${baseUrl}/api/payments/vnpay-return` },
-      { method: "POST", path: `${baseUrl}/api/payments/create-vnpay-payment` },
-      
-      //product
-      { method: "GET", path: `${baseUrl}/api/products/list` },
-      { method: "GET", path: `${baseUrl}/api/products/seller-list` },
-      { method: "GET", path: `${baseUrl}/api/products/category/:id` },
-      { method: "GET", path: `${baseUrl}/api/products/brand/:id` },
-      { method: "GET", path: `${baseUrl}/api/products/filter` },
-      { method: "GET", path: `${baseUrl}/api/products/search` },
-      { method: "GET", path: `${baseUrl}/api/products/:id` },
-      { method: "POST", path: `${baseUrl}/api/products/add` },
-      { method: "PUT", path: `${baseUrl}/api/products/edit/:id` },
-      { method: "PUT", path: `${baseUrl}/api/products/update/:id` },
-      { method: "DELETE", path: `${baseUrl}/api/products/delete/:id` },
-      
-      //review
-      { method: "GET", path: `${baseUrl}/api/reviews/all` },
-      { method: "GET", path: `${baseUrl}/api/reviews/product/:productId` },
-      { method: "POST", path: `${baseUrl}/api/reviews/product` },
-      
-      //user
-      { method: "GET", path: `${baseUrl}/api/users/data` },
-      { method: "GET", path: `${baseUrl}/api/users/get-addresses` },
-      { method: "GET", path: `${baseUrl}/api/users/cart` },
-      { method: "GET", path: `${baseUrl}/api/users/seller-list` },
-      { method: "POST", path: `${baseUrl}/api/users/add-address` },
-      { method: "POST", path: `${baseUrl}/api/users/cart/update` },
-      { method: "PUT", path: `${baseUrl}/api/users/update-role/:userId` },
-      { method: "PUT", path: `${baseUrl}/api/users/toggle-block/:userId` },
-      
-      //userCoupon
-      { method: "GET", path: `${baseUrl}/api/user-coupons/my` },
-      { method: "POST", path: `${baseUrl}/api/user-coupons/claim` },
+      {
+        name: "luckyDraw",
+        route: [
+          // { method: "POST", path: `${baseUrl}/api/luckyDraws/spin` },
+        ]
+      },
 
+      {
+        name: "openrouter",
+        route: [
+          { method: "POST", path: `${baseUrl}/api/openrouter/chat` },
+        ]
+      },
+      
+      {
+        name: "order",
+        route: [
+          { method: "GET", path: `${baseUrl}/api/orders/list` },
+          { method: "GET", path: `${baseUrl}/api/orders/pending` },
+          { method: "GET", path: `${baseUrl}/api/orders/processing` },
+          { method: "GET", path: `${baseUrl}/api/orders/delivered` },
+          { method: "GET", path: `${baseUrl}/api/orders/cancelled` },
+          { method: "GET", path: `${baseUrl}/api/orders/detail/:id` },
+          { method: "GET", path: `${baseUrl}/api/orders/seller-orders` },
+          { method: "GET", path: `${baseUrl}/api/orders/check-purchase/:productId` },
+          { method: "POST", path: `${baseUrl}/api/orders/create` },
+          { method: "POST", path: `${baseUrl}/api/orders/verify-payment` },
+          { method: "PUT", path: `${baseUrl}/api/orders/cancel/:id` },
+          { method: "PUT", path: `${baseUrl}/api/orders/update-status/:id` },
+        ]
+      },
+      
+      {
+        name: "payment",
+        route: [
+          { method: "GET", path: `${baseUrl}/api/payments/vnpay-return` },
+          { method: "POST", path: `${baseUrl}/api/payments/create-vnpay-payment` },
+        ]
+      },
 
+      {
+        name: "product",
+        route: [
+          { method: "GET", path: `${baseUrl}/api/products/list` },
+          { method: "GET", path: `${baseUrl}/api/products/seller-list` },
+          { method: "GET", path: `${baseUrl}/api/products/category/:id` },
+          { method: "GET", path: `${baseUrl}/api/products/brand/:id` },
+          { method: "GET", path: `${baseUrl}/api/products/filter` },
+          { method: "GET", path: `${baseUrl}/api/products/search` },
+          { method: "GET", path: `${baseUrl}/api/products/:id` },
+          { method: "POST", path: `${baseUrl}/api/products/add` },
+          { method: "PUT", path: `${baseUrl}/api/products/edit/:id` },
+          { method: "PUT", path: `${baseUrl}/api/products/update/:id` },
+          { method: "DELETE", path: `${baseUrl}/api/products/delete/:id` },
+        ]
+      },
+
+      {
+        name: "review",
+        route: [
+          { method: "GET", path: `${baseUrl}/api/reviews/all` },
+          { method: "GET", path: `${baseUrl}/api/reviews/product/:productId` },
+          { method: "POST", path: `${baseUrl}/api/reviews/product` },
+        ]
+      },
+
+      {
+        name: "user",
+        route: [
+          { method: "GET", path: `${baseUrl}/api/users/data` },
+          { method: "GET", path: `${baseUrl}/api/users/get-addresses` },
+          { method: "GET", path: `${baseUrl}/api/users/cart` },
+          { method: "GET", path: `${baseUrl}/api/users/seller-list` },
+          { method: "POST", path: `${baseUrl}/api/users/add-address` },
+          { method: "POST", path: `${baseUrl}/api/users/cart/update` },
+          { method: "PUT", path: `${baseUrl}/api/users/update-role/:userId` },
+          { method: "PUT", path: `${baseUrl}/api/users/toggle-block/:userId` },
+        ]
+      },
+
+      {
+        name: "userCoupon",
+        route: [
+          { method: "GET", path: `${baseUrl}/api/user-coupons/my` },
+          { method: "POST", path: `${baseUrl}/api/user-coupons/claim` },
+        ]
+      },
+      
     ],
   });
 });
